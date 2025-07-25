de7e5ee83fcabf271eb67927f977a9cf30c4a446
jonathab <jonathanbeni1234@gmail.com>
2025-07-25 14:08:56 +0200

    debuggin

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,41 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './db/Connection.js';
-import authRouter from './routes/authRoutes.js';
-import categoryRouter from './routes/categoryRoutes.js';
-import paymentReportRouter from './routes/paymentReportRoutes.js';
-import memberRouter from './routes/memberRoutes.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/Connection.js";
+import authRouter from "./routes/authRoutes.js";
+import categoryRouter from "./routes/categoryRoutes.js";
+import paymentReportRouter from "./routes/paymentReportRoutes.js";
+import memberRouter from "./routes/memberRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
-const PORT = process.env.PORT|| 3000
-const app = express()
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+connectDB();
 
 // middlewares
-app.use(express.json())
+app.use(express.json());
 
 // cors headers
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
-}))
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ success: true, message: "Backend reachable!" });
 });
 
-
 // routes
-app.use('/api/auth/v1/', authRouter)
-app.use('/api/category/v1/', categoryRouter)
-app.use('/api/member/v1/', memberRouter)
-app.use('/api/payment/report/v1/', paymentReportRouter)
+app.use("/api/auth/v1/", authRouter);
+app.use("/api/category/v1/", categoryRouter);
+app.use("/api/member/v1/", memberRouter);
+app.use("/api/payment/report/v1/", paymentReportRouter);
 
-app.listen(PORT, '0.0.0.0', () => {
-  connectDB();
-  console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
