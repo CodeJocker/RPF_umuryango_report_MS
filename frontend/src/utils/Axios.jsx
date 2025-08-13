b02ef6556837f50f9dd83ea77c12050cdbe7500c
jonathab <jonathanbeni1234@gmail.com>
2025-08-13 18:33:52 +0200

    attempt 2 of debugging

--- frontend/src/utils/Axios.jsx
+++ frontend/src/utils/Axios.jsx
@@ -1,42 +1,42 @@
-import axios from "axios";
+  import axios from "axios";
 
-const token = localStorage.getItem("user"); // or whatever key stores your token
+  const token = localStorage.getItem("user"); // or whatever key stores your token
+console.log(import.meta.env.VITE_API_URL);
+  export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "https://umuryangorpf.vercel.app/api",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    withCredentials: true,
+    timeout: 10000,
+  });
 
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://umuryangorpf.vercel.app/api",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    Authorization: `Bearer ${token}`,
-  },
-  withCredentials: true,
-  timeout: 10000,
-});
 
 
+  api.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("user");
+      console.log(token)
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("user");
-    console.log(token)
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// // Add request interceptor for dynamic token
-// api.interceptors.request.use(
-//   config => {
-//   if (token) {
-//     config.headers.authorization = `Bearer ${token}`;
-//   }
-//   return config;
-//   },
-//   error => {
-//     console.log("Request error", error)
-//     return Promise.reject(error);
-//   }
-// );
+  // // Add request interceptor for dynamic token
+  // api.interceptors.request.use(
+  //   config => {
+  //   if (token) {
+  //     config.headers.authorization = `Bearer ${token}`;
+  //   }
+  //   return config;
+  //   },
+  //   error => {
+  //     console.log("Request error", error)
+  //     return Promise.reject(error);
+  //   }
+  // );
