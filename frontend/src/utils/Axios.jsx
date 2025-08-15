5040318c7b44b9c040b45618a10d0d00f9e7dbea
jonathab <jonathanbeni1234@gmail.com>
2025-08-15 12:28:02 +0200

    attempt 3

--- frontend/src/utils/Axios.jsx
+++ frontend/src/utils/Axios.jsx
@@ -17,7 +17,7 @@ console.log(import.meta.env.VITE_API_URL);
 
   api.interceptors.request.use(
     (config) => {
-      const token = localStorage.getItem("user");
+      // const token = localStorage.getItem("user");
       console.log(token)
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
